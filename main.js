c0b46fdaf60e4e34fdf6eedc015e53ea80b8f303
Ibrahim kamal <150850977+Ibrahimkamal5@users.noreply.github.com>
2025-08-27 19:14:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function ReverseCharacters(str) {
+    let reversed = "";
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+
+    return reversed;
+}
+
+console.log(ReverseCharacters("Hello, World!")); // Output: "!dlroW ,olleH"
+console.log(ReverseCharacters("javascript")); // "tpircsavaj"
+console.log(ReverseCharacters("elyts"));      // "style"
+console.log(ReverseCharacters("nadroj"));     // "jordan"
+console.log(ReverseCharacters("abc564"));     // "465cba"
+
